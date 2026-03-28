2e39bcf0a57b3f46dd28ceddccf3e5ab2946ba19
fjoao <fjoao9744@gmail.com>
2026-03-28 13:56:39 -0300

    topics and hours_register

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
-from .views import dashboard
+from .views import dashboard, register_hours
 from django.urls import path
 
 urlpatterns = [
-    path('', dashboard, name="dashboard")
+    path('', dashboard, name="dashboard"),
+    path('register/', register_hours, name="register_hours")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic, Student, Register
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
-    Topic.objects.create(name="test")
     return render(request, "index.html")
+
+@login_required
+def register_hours(request):
+    if request.method == "POST":
+        topic = request.POST["topic"]
+        hours = request.POST["hours"]
+        student = Student.objects.create(user=request.user, total_hours=hours)
+        if not Topic.objects.filter(name=topic).exists():
+            topic = Topic.objects.create(name=topic)
+        else:
+            topic = Topic.objects.get(name=topic)
+        Register.objects.create(student=student, topic=topic, hours=hours)
+
+        return redirect("dashboard")
+
+    return render(request, "register_hours.html")

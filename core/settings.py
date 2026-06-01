cd2b0b56700011d0fefea453a560729052476d8d
fjoao <fjoao9744@gmail.com>
2026-06-01 18:06:00 -0300

    whitenoise and admin fix

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 import os
 
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: render(request, 'index.html'), name="homepage"),
@@ -10,3 +11,4 @@ urlpatterns = [
     path('auth/', include("authentication.urls")),
     path('api/', include("dashboard_API.urls"))
 ]
+
